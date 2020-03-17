2a12150e1f5683963159cec8aa25e93c0e882a32
Ali Waqas <aliwaqas@codeforpakistan.org>
2020-03-17 14:00:56 +0500

    .

---
+++ build/precache-manifest.0b8fea418173edf9c1f9e1625fc9eb97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53da2140e9225f872dbe06fa3ad1c096",
+    "url": "/sorting-algorithms/index.html"
+  },
+  {
+    "revision": "39ccbdd240be5d254b06",
+    "url": "/sorting-algorithms/static/css/main.96b6399f.chunk.css"
+  },
+  {
+    "revision": "9ff33724b34823bc0e9a",
+    "url": "/sorting-algorithms/static/js/2.e19d2b41.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/sorting-algorithms/static/js/2.e19d2b41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39ccbdd240be5d254b06",
+    "url": "/sorting-algorithms/static/js/main.19a1e0f5.chunk.js"
+  },
+  {
+    "revision": "5d0e1cb5957fa706b48f",
+    "url": "/sorting-algorithms/static/js/runtime-main.2450d311.js"
+  }
+]);
