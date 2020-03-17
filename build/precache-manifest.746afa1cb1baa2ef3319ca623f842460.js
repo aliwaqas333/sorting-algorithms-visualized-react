4d2982571a3248b5a2bbc0be2b2a15516181e296
aliwaqas333 <aliwaqas@codeforpakistan.org>
2020-03-17 13:14:47 +0500

    build version

---
+++ build/precache-manifest.746afa1cb1baa2ef3319ca623f842460.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1658ffd4b570a6eb1050df3fae56e550",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37d849ca5770dc9dc88f",
+    "url": "/static/css/main.a08091e5.chunk.css"
+  },
+  {
+    "revision": "9ff33724b34823bc0e9a",
+    "url": "/static/js/2.e19d2b41.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.e19d2b41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37d849ca5770dc9dc88f",
+    "url": "/static/js/main.cfdfb943.chunk.js"
+  },
+  {
+    "revision": "916e972d456548fa5b64",
+    "url": "/static/js/runtime-main.1167a928.js"
+  }
+]);
