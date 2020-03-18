5889508a1a1085d23d5649de10ce55df59040ee9
aliwaqas333 <aliwaqas@codeforpakistan.org>
2020-03-18 12:43:28 +0500

    Updates

---
+++ precache-manifest.faaf3d4234de29f7c3c68123012152e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d2f17f7bcf69497cc76eb40aa2efb62",
+    "url": "/sorting-algorithms/index.html"
+  },
+  {
+    "revision": "24fb168358c869dce29a",
+    "url": "/sorting-algorithms/static/css/main.a907c929.chunk.css"
+  },
+  {
+    "revision": "87b9a61042e31206f255",
+    "url": "/sorting-algorithms/static/js/2.456b73c8.chunk.js"
+  },
+  {
+    "revision": "a5288a4ed4e4beba8413aa5c9ae969ea",
+    "url": "/sorting-algorithms/static/js/2.456b73c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24fb168358c869dce29a",
+    "url": "/sorting-algorithms/static/js/main.7fc8c256.chunk.js"
+  },
+  {
+    "revision": "5d0e1cb5957fa706b48f",
+    "url": "/sorting-algorithms/static/js/runtime-main.2450d311.js"
+  }
+]);
