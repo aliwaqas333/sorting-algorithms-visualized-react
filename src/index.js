a2541a4a2aa45158cc80eb805645f9318b8bd47b
Ali Waqas <engrr.ali.waqas@gmail.com>
2023-11-05 14:22:22 -0500

    deploy

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './css/bootstrap.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import './css/columns.css'
-import {
-    BrowserRouter as Router,
-    Route
-} from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./css/bootstrap.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import "./css/columns.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-
-ReactDOM.render(<Router basename='/sorting-algorithms'><App /></Router>, document.getElementById('root'));
+ReactDOM.render(
+  <Router basename="/">
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
